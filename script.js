6a72a755ea58384e47dc55e3853e1e232c4ad577
SvitlanaSuslenkova <136314004+SvitlanaSuslenkova@users.noreply.github.com>
2023-10-20 15:08:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 let menu= document.querySelector(".menuImg");
 let menutext= document.querySelector(".menuText");
 let close = document.querySelector(".x");
+let grey= document.querySelector(".grey");
+
 var x = window.matchMedia("(min-width: 798px)");
 
  menu.addEventListener ("click", function(){
 menutext.classList.replace("menuText","menuTextmobile");
+grey.style.display="block";
     
 });
 close.addEventListener ("click", function(){
     menutext.classList.replace("menuTextmobile","menuText");
+    grey.style.display="none";
 });
 
 document.addEventListener ("click", function(event){
     if (menutext !=event.target && menu != event.target ) {
         menutext.classList.replace("menuTextmobile","menuText");
+        grey.style.display="none";
       }   
 });
 
 window.addEventListener("resize", function(){
     if (x.matches) {
-        menutext.classList.replace("menuTextmobile","menuText");   
+        menutext.classList.replace("menuTextmobile","menuText");  
+        grey.style.display="none"; 
     }
 });   
